6dcf7789517cc02d9bf1903d27c4e4cb5c21a560
Fauzi <fauzibinfaisal@gmail.com>
2019-08-23 16:05:05 +0700

    Initial Commit

---
+++ MC3login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MC3login
+//
+//  Created by Fauzi Fauzi on 23/08/19.
+//  Copyright © 2019 fauzify. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
