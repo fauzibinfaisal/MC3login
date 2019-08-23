4a3f76b615bbb2a62be6c702d5958c72e0989437
Fauzi <fauzibinfaisal@gmail.com>
2019-08-23 19:53:55 +0700

    initial commit

---
+++ MC3login/CheckButton.swift
@@ -0,0 +1,21 @@
+//
+//  CheckButton.swift
+//  MC3login
+//
+//  Created by Fauzi Fauzi on 23/08/19.
+//  Copyright © 2019 fauzify. All rights reserved.
+//
+
+import UIKit
+
+class CheckButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ MC3login/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  MC3login
+//
+//  Created by Fauzi Fauzi on 23/08/19.
+//  Copyright © 2019 fauzify. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
