893a1ba1f24cb65834d7281b5c92524240226fc8
Fauzi <fauzibinfaisal@gmail.com>
2019-08-26 14:15:28 +0700

    tambah edit cat

---
+++ MC3login/Controller/CatEditViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CatEditViewController.swift
+//  MC3login
+//
+//  Created by Fauzi Fauzi on 26/08/19.
+//  Copyright © 2019 fauzify. All rights reserved.
+//
+
+import UIKit
+
+class CatEditViewController: UIViewController {
+
+    @IBOutlet weak var editCatTableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MC3login/View/PhotoGalleryViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoGalleryViewCell.swift
+//  MC3login
+//
+//  Created by Fauzi Fauzi on 26/08/19.
+//  Copyright © 2019 fauzify. All rights reserved.
+//
+
+import UIKit
+
+class PhotoGalleryViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
